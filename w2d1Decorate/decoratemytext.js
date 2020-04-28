add1621ae6c7dd77aa8be9a0818edf0c6c915f37
wintesfamariam <wintesfamariam@gmail.com>
2020-04-27 21:29:48 -0500

    updated

--- w2d1Decorate/decoratemytext.js
+++ w2d1Decorate/decoratemytext.js
@@ -1,20 +1,21 @@
 /*jshint esversion: 6 */
 
 
-
     window.onload = function(){
     "use strict";
         //hello world ALERT BUTTON
     let bigger_decorations_btn = document.getElementById("bigger_decorations");
-    bigger_decorations_btn.onclick = alertHelloWorld;
+   // bigger_decorations_btn.onclick = alertHelloWorld;   //ALERT HELLO WORLD
+        bigger_decorations_btn.onclick = changeTestSize;
 
     function alertHelloWorld(){
         alert("hello world");
     }
 
-
-
-
+    function changeTestSize() {
+        let textarea = document.getElementById("myText");
+        textarea.style.fontSize = "24pt";
+    }
 
 
     };
